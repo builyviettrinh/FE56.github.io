9df48f1351d7100a1a076152d2cbfc8a43b77d03
builyviettrinh <builyviettrinh@gmail.com>
2020-11-29 16:31:55 +0700

    update bt2

--- JavaScript/JS_Buoi3/js/bt1/main_OOP.js
+++ JavaScript/JS_Buoi3/js/bt1/main_OOP.js
@@ -3,9 +3,53 @@ function hienThiThongTin(){
     lấy info user nhập vào 5 ô input
     */
 
-   var maSV = document.getElementById("txtMaSV").value;
-   var tenSV = document.getElementById("txtTenSV").value;
-   var loaiSV = document.getElementById("loaiSV").value;
-   var diemToan = document.getElementById("txtDiemToan").value;
-   var diemVan = document.getElementById("txtDiemVan").value;
+   var _maSV = document.getElementById("txtMaSV").value;
+   var _tenSV = document.getElementById("txtTenSV").value;
+   var _loaiSV = document.getElementById("loaiSV").value;
+   var _diemToan = document.getElementById("txtDiemToan").value;
+   var _diemVan = document.getElementById("txtDiemVan").value;
+
+
+/*
+tạo đối tượng sinh viên
+*/
+    var sinhVien = {
+        // key: value 
+        // thuộc tính
+        maSV:_maSV,
+        tenSV: _tenSV,
+        loaiSV: _loaiSV,
+        diemToan: _diemToan,
+        diemVan: _diemVan,
+
+        // Phương thức 
+        tinhDTB: function(){
+            var dtb = (parseFloat(this.diemToan) +parseFloat(this.diemVan) )/2;
+            return dtb;
+        },
+
+        xepLoai: function(dtb){
+            var xepLoai = "";
+            if(dtb <= 5 ){
+                xepLoai = "yeu";
+            }
+            else if(dtb < 8){
+                xepLoai = "kha";
+            }
+            else if(dtb <= 10){
+                xepLoai = "gioi";         
+            }
+            return xepLoai;
+        }
+    };
+
+     /*
+     hiển thị thông tin
+    */ 
+   var diemTB = sinhVien.tinhDTB();
+   document.getElementById("spanTenSV").innerHTML = sinhVien.tenSV;
+   document.getElementById("spanMaSV").innerHTML = sinhVien.maSV;
+   document.getElementById("spanLoaiSV").innerHTML = sinhVien.loaiSV;
+   document.getElementById("spanDTB").innerHTML = diemTB;   
+   document.getElementById("spanXepLoai").innerHTML = sinhVien.xepLoai(diemTB);
 }
