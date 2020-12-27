9307bbac5dbd651489f37b5884049ea476863b1f
builyviettrinh <builyviettrinh@gmail.com>
2020-12-27 23:25:12 +0700

    update

---
+++ React/fe56reactjsThay/src/baitap2/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./header";
+import Carousel from "./carousel";
+import WhatWeDo from "./what-we-do";
+import Contact from "./contact";
+import Listcard from "./list-card";
+import Footer from "./footer";
+
+function Baitap2(){
+    return (
+        <div>
+            <Header />
+            <Carousel />
+            <div className="row">
+                <WhatWeDo />
+                <Contact />
+            </div>
+            <Listcard />
+            <Footer />
+        </div>
+    )
+}
+
+export default Baitap2;
---
+++ React/fe56reactjsThay/src/baitap3/list-smartphone.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Product from "./product";
+
+export default function ListSmartphone() {
+  return (
+    <section
+      id="smartphone"
+      className="container-fluid pt-5 pb-5 bg-light text-dark"
+    >
+      <h1 className="text-center">BEST SMARTPHONE</h1>
+      <div className="row">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </section>
+  );
+}
